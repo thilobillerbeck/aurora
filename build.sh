45ef7d97446fb0a2bb36905747869af4f873f38d
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2025-02-11 01:42:53 +0100

    remove mullvad

--- build.sh
+++ build.sh
@@ -21,6 +21,5 @@ set -ouex pipefail
 
 #### Example for enabling a System Unit File
 dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf -y config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/stable/mullvad.repo
-dnf -y install discord mullvad-vpn
+dnf -y install discord
 systemctl enable podman.socket
